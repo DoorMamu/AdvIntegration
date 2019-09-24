5475ad3e07c1f5c1cbae92f90a6ca5dc774eecdc
DoorMamu <mmkkolhapur@gmail.com>
2019-09-24 18:44:42 +0530

    mail,mob,aadhar

--- app/src/main/java/com/example/advintegration/BankDB.java
+++ app/src/main/java/com/example/advintegration/BankDB.java
@@ -10,7 +10,7 @@ SQLiteDatabase db;
         db.execSQL("create table if not exists customer(" +
                 "cust_id int(5000)  not null ,cust_fname varchar(60),cust_mname varchar(60)," +
                 "cust_lname varchar(60),cust_addr varchar(256),cust_dist varchar(60)," +
-                "cust_state varchar(60),cust_zip varchar(60),cust_mail varchar(255)," +
+                "cust_state varchar(60),cust_mail varchar(255)," +
                 "cust_mob int(10),cust_aadhar varchar(12)," +
                 "cust_dob date,primary key(cust_id) )");
 //db.execSQL("CREATE TABLE IF NOT EXISTS student(rollno VARCHAR,name VARCHAR,marks VARCHAR);");
